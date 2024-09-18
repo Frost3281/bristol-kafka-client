96c16ef30226e794659d5441eb704d1e85aa2603
Frost3281 <Vitaliy.Mikhalchenko@bristolcapital.ru>
2024-09-18 17:29:28 +0300

    add message producing service

--- bristol_kafka_client/message_producing.py
+++ bristol_kafka_client/message_producing.py
@@ -1,24 +1,28 @@
 import json
 from typing import Any, Literal, Protocol, Sequence, Union
 
+from pydantic.main import IncEx
+
 
 class KafkaData(Protocol):
     """Данные для отправки в Kafka."""
 
-    def model_dump(  # noqa: PLR0913
+    def model_dump_json(  # noqa: PLR0913
         self,
         *,
-        mode: Union[Literal['json', 'python'], str] = 'python',  # noqa: PYI051
-        include: Union[set[str], None] = None,
-        exclude: Union[set[str], None] = None,
-        by_alias: bool = False,
+        indent: int | None = None,
+        include: IncEx = None,
+        exclude: IncEx = None,
+        context: Any | None = None,  # noqa: ANN401
+        by_alias: bool = True,
         exclude_unset: bool = False,
         exclude_defaults: bool = False,
         exclude_none: bool = False,
         round_trip: bool = False,
-        warnings: bool = True,
-    ) -> dict[str, Any]:
-        """Метод для преобразования в словарь."""
+        warnings: bool | Literal['none', 'warn', 'error'] = True,
+        serialize_as_any: bool = False,
+    ) -> str:
+        """Метод для сериализации модели."""
 
 
 class KafkaProducer(Protocol):
@@ -52,11 +56,12 @@ async def post_to_kafka(
     data_to_send: Sequence[KafkaData],
     topic: str,
     key: Union[str, None] = None,
+    **dump_kwargs: Any,  # noqa: ANN401
 ) -> None:
     """Отправляем данные в Kafka."""
     await producer.send(
         topic,
-        _transform_models_for_kafka(data_to_send),
+        _transform_models_for_kafka(data_to_send, **dump_kwargs),
         key=_to_bytes(key),
     )
 
@@ -65,9 +70,11 @@ def _to_bytes(string_to_encode: Union[str, None]) -> Union[bytes, None]:
     return bytes(string_to_encode, 'utf-8') if string_to_encode else None
 
 
-def _transform_models_for_kafka(data_to_send: Sequence[KafkaData]) -> Union[bytes, None]:
+def _transform_models_for_kafka(
+    data_to_send: Sequence[KafkaData], **dump_kwargs: Any,  # noqa: ANN401
+) -> Union[bytes, None]:
     json_to_send = json.dumps(
-        [item_to_send.model_dump() for item_to_send in data_to_send],
+        [item_to_send.model_dump_json(**dump_kwargs) for item_to_send in data_to_send],
         indent=4,
         default=str,
         ensure_ascii=False,
